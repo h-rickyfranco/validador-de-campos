3dea236019b0f090bf26d784b4a8750aa770756d
Henrique Franco <hrick_gtr@hotmail.com>
2023-04-19 11:10:01 -0300

    final

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
 import './style.css';
+import validator from 'validator';
+
+const campoDeTexto = document.getElementById('value');
+const button = document.getElementById('button');
+const seletor = document.getElementById('option');
+const textoDeSaida = document.getElementById('answer');
+
+const UUI_ID = 4;
+
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const campos = {
+    cpf: validator.isTaxID(campoDeTexto.value, 'pt-BR'),
+    hexColor: validator.isHexColor(campoDeTexto.value),
+    email: validator.isEmail(campoDeTexto.value),
+    uuid: validator.isUUID(campoDeTexto.value, UUI_ID),
+    url: validator.isURL(campoDeTexto.value),
+  };
+  textoDeSaida.innerHTML = `A validação retornou ${campos[seletor.value]}`;
+});
